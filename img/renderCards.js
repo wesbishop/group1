eda5d34821dc8c8f4a8b83d6624d9d0228140f62
CMichels <chrisjohnmichels@gmail.com>
2018-05-09 10:32:20 -0400

    Create renderCards.js

---
+++ img/renderCards.js
@@ -0,0 +1,21 @@
+function renderCards(data){
+  var cardHTML = '';
+  let end = Math.round((new Date()).getTime() / 1000);
+  let start = end - (86400* 2);
+  let prices = [];
+  let text = 'text-dark';
+  data.forEach(function(el){
+    let url = `https://poloniex.com/public?command=returnChartData&currencyPair=USDT_${el.pair}&start=${start}&end=${end}&period=86400`
+    $.get(url).then((data)=>{
+      if(data[1].close > data[0].close){
+        text = 'text-success';
+      }else{
+        text = 'text-danger';
+      }
+      cardHTML = `<div class='card bg-dark' style="width: 18rem;"><div class='card-body'><h5 class='card-title ${text}'>${el.pair} ${data[1].close}</h5>
+                <p class ='card-text text-light'>Your Total = $${Math.floor(Number(el.amount * data[1].close))}</p></div></div>`
+      $('.portCard').append(cardHTML)
+    });
+  })
+
+}
