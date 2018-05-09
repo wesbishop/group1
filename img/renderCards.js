250319c46414f8b4a026966b588484837fb73500
CMichels <chrisjohnmichels@gmail.com>
2018-05-09 11:50:25 -0400

    Update renderCards.js

--- img/renderCards.js
+++ img/renderCards.js
@@ -15,10 +15,9 @@ function renderCards(data){
         text = 'text-danger';
       }
       percent =((data[1].close - data[0].close)/data[0].close * 100)
-      cardHTML = `<div class='card bg-dark' style="width: 15rem;"><div class='card-body'><h6 class='card-title ${text}'>${el.pair} ${data[1].close.toFixed(3)}    ${percent.toFixed(2)}%</h6>
+      cardHTML = `<div class='card bg-dark col-2'><div class='card-body'><h6 class='card-title ${text}'>${el.pair} ${data[1].close.toFixed(3)}    ${percent.toFixed(2)}%</h6>
                 <p class ='card-text text-light'>Your Total = $${Math.floor(Number(el.amount * data[1].close))}</p></div></div>`
       $('.portCard').append(cardHTML)
     });
   })
-
 }
