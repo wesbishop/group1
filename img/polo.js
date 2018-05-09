3d01fddbab209222aa99912476c42a77e84e8154
CMichels <chrisjohnmichels@gmail.com>
2018-05-09 09:11:20 -0400

    Update polo.js

--- img/polo.js
+++ img/polo.js
@@ -1,13 +1,17 @@
 class poloUrl {
-    constructor(pair, period, periodRepeat) {
+    constructor(pair, period, periodRepeat, color) {
         this.pair = pair;
         this.close = [];
+        this.closeColor =[];
+        this.color = color || 'white';
         this.periodRepeat = periodRepeat || 500
         this.date = [];
         this.end = Math.round((new Date()).getTime() / 1000);
         this.period = period;
         this.start = this.end - (this.period * this.periodRepeat);
+        this.colorStart = this.end - (this.period * 2);
         this.url = `https://poloniex.com/public?command=returnChartData&currencyPair=USDT_${pair}&start=${this.start}&end=${this.end}&period=${this.period}`;
+        this.colorUrl = `https://poloniex.com/public?command=returnChartData&currencyPair=USDT_${pair}&start=${this.colorStart}&end=${this.end}&period=86400`;
     }
     timeConverter(UNIX_timestamp) {
         var a = new Date(UNIX_timestamp * 1000);
@@ -29,7 +33,23 @@ class poloUrl {
                 this.date.push(this.timeConverter(data[i]['date']));
             }
             this.renderChart();
+        })
+    }
 
+    colorPick() {
+        $.get(this.colorUrl).then((data)=>{
+              for (var i = 0; i < data.length; i++) {
+              this.closeColor.push(data[i]['close']);
+          }
+          if(this.closeColor[1] > this.closeColor[0]){
+            this.color = 'green';
+            $('#makePair').attr('color', 'green');
+            return('green')
+          }else{
+            this.color = 'red';
+            $('#makePair').attr('color', 'red');
+            return('red')
+          }
         })
     }
 
